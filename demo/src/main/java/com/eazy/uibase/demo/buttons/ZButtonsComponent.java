b809deeca60b050591a9556a0ddc4e1afcd60904
yuailong@tal.com <yuailong@tal.com>
2021-02-22 16:56:11 +0800

    First commit for Android's  ui base

---
+++ demo/src/main/java/com/eazy/uibase/demo/buttons/ZButtonsComponent.java
@@ -0,0 +1,43 @@
+package com.eazy.uibase.demo.buttons;
+
+import com.google.auto.service.AutoService;
+import com.eazy.uibase.demo.R;
+import com.eazy.uibase.demo.core.Component;
+import com.eazy.uibase.demo.core.ComponentFragment;
+import com.eazy.uibase.demo.core.annotation.Author;
+
+@AutoService(Component.class)
+@Author("cmguo")
+public class ZButtonsComponent implements Component{
+    @Override
+    public int id() {
+        return R.id.component_buttons;
+    }
+
+    @Override
+    public int group() {
+        return R.string.group_styles;
+    }
+
+    @Override
+    public int icon() {
+        return android.R.drawable.btn_plus;
+    }
+
+    @Override
+    public int title() {
+        return R.string.component_buttons;
+    }
+
+    @Override
+    public int description() {
+        return R.string.component_buttons_desc;
+    }
+
+    @Override
+    public Class<? extends ComponentFragment> fragmentClass() {
+        return ButtonsFragment.class;
+    }
+
+}
+
