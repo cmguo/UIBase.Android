4d58850066c55bb4a212bd634f3653e51f7285aa
cmguo <357215492@qq.com>
2021-03-20 21:43:00 +0800

    simple components

---
+++ demo/src/main/java/com/xhb/uibase/demo/components/simple/XHBToolTipComponent.kt
@@ -0,0 +1,35 @@
+package com.xhb.uibase.demo.components.simple
+
+import com.google.auto.service.AutoService
+import com.xhb.uibase.demo.R
+import com.xhb.uibase.demo.core.Component
+import com.xhb.uibase.demo.core.ComponentFragment
+import com.xhb.uibase.demo.core.annotation.Author
+
+@AutoService(Component::class)
+@Author("cmguo")
+class XHBToolTipComponent : Component {
+    override fun id(): Int {
+        return R.id.component_xhb_tool_tips
+    }
+
+    override fun group(): Int {
+        return R.string.group_simple
+    }
+
+    override fun icon(): Int {
+        return android.R.drawable.btn_plus
+    }
+
+    override fun title(): Int {
+        return R.string.component_xhb_tool_tips
+    }
+
+    override fun description(): Int {
+        return R.string.component_xhb_tool_tips_desc
+    }
+
+    override fun fragmentClass(): Class<out ComponentFragment<*, *, *>?> {
+        return XHBToolTipFragment::class.java
+    }
+}
