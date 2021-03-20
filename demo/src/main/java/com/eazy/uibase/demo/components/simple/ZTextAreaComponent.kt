69fa1ccf0226d39fdb72bc1283845d3668da7c0e
cmguo <357215492@qq.com>
2021-03-20 21:43:00 +0800

    simple components

---
+++ demo/src/main/java/com/eazy/uibase/demo/components/simple/ZTextAreaComponent.kt
@@ -0,0 +1,35 @@
+package com.eazy.uibase.demo.components.simple
+
+import com.google.auto.service.AutoService
+import com.eazy.uibase.demo.R
+import com.eazy.uibase.demo.core.Component
+import com.eazy.uibase.demo.core.ComponentFragment
+import com.eazy.uibase.demo.core.annotation.Author
+
+@AutoService(Component::class)
+@Author("cmguo")
+class ZTextAreaComponent : Component {
+    override fun id(): Int {
+        return R.id.component_z_text_areas
+    }
+
+    override fun group(): Int {
+        return R.string.group_simple
+    }
+
+    override fun icon(): Int {
+        return android.R.drawable.btn_plus
+    }
+
+    override fun title(): Int {
+        return R.string.component_z_text_areas
+    }
+
+    override fun description(): Int {
+        return R.string.component_z_text_areas_desc
+    }
+
+    override fun fragmentClass(): Class<out ComponentFragment<*, *, *>?> {
+        return ZTextAreaFragment::class.java
+    }
+}
---
+++ demo/src/main/java/com/eazy/uibase/demo/components/simple/ZTextAreaFragment.kt
@@ -0,0 +1,70 @@
+package com.eazy.uibase.demo.components.simple
+
+import android.os.Bundle
+import androidx.databinding.Bindable
+import com.eazy.uibase.demo.core.ComponentFragment
+import com.eazy.uibase.demo.core.SkinManager
+import com.eazy.uibase.demo.core.ViewModel
+import com.eazy.uibase.demo.core.ViewStyles
+import com.eazy.uibase.demo.core.style.IconStyle
+import com.eazy.uibase.demo.core.style.annotation.*
+import com.eazy.uibase.demo.databinding.CompoundButtonFragmentBinding
+import com.eazy.uibase.demo.databinding.TextAreaFragmentBinding
+import skin.support.observe.SkinObservable
+import skin.support.observe.SkinObserver
+
+class ZTextAreaFragment : ComponentFragment<TextAreaFragmentBinding?, ZTextAreaFragment.Model?, ZTextAreaFragment.Styles?>(), SkinObserver {
+
+    class Model(fragment: ZTextAreaFragment?) : ViewModel() {
+    }
+
+    class Styles(fragment: ZTextAreaFragment?) : ViewStyles() {
+
+        @Bindable
+        @Title("最大字数") @Description("设为0不限制；如果有限制，将展现字数指示")
+        var maximumCharCount = 100
+
+        @Bindable
+        @Title("最小高度") @Description("没有文字时的高度")
+        var minimunHeight = 100f
+
+        @Bindable
+        @Title("最大高度") @Description("高度随文字变化，需要指定最大高度；包含字数指示（如果有的话）")
+        var maximunHeight = 300f
+
+        @Bindable
+        @Title("占位文字") @Description("没有任何输入文字时，显示的占位文字（灰色）")
+        var placeholder = "请输入"
+
+        @Bindable
+        @Title("显示边框") @Description("设为0不限制；如果有限制，将展现字数指示")
+        var showBorder = false
+
+        @Bindable
+        @Title("左图标") @Description("设置左边的图标，资源ID类型，控件内部会自动重新布局")
+        @Style(IconStyle::class)
+        var leftIcon = 0
+
+        @Bindable
+        @Title("右图标") @Description("设置右边的图标，资源ID类型，控件内部会自动重新布局")
+        @Style(IconStyle::class)
+        var rightIcon = 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SkinManager.addObserver(this)
+    }
+
+    override fun onDestroy() {
+        SkinManager.removeObserver(this)
+        super.onDestroy()
+    }
+
+    override fun updateSkin(observable: SkinObservable, o: Any) {
+    }
+
+    companion object {
+        private const val TAG = "ZTextAreaFragment"
+    }
+}
