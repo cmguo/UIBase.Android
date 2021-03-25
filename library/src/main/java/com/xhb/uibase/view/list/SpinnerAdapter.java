2308560ebeb30038e9557f79459f8876b478ad84
guochunmao <guochunmao@100tal.com>
2021-03-25 16:15:12 +0800

    SpinnerAdapter: split from binding adapter

---
+++ library/src/main/java/com/xhb/uibase/view/list/SpinnerAdapter.java
@@ -0,0 +1,22 @@
+package com.xhb.uibase.view.list;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SpinnerAdapter<T> extends ArrayAdapter implements android.widget.SpinnerAdapter {
+
+    public SpinnerAdapter(@NonNull Context context) {
+        super(context, 0);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getDropDownView(position, convertView, parent);
+    }
+}
