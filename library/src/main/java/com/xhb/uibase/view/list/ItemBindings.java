27bed728ac8df29e0da6575d5ea6417a22f97c9c
guochunmao <guochunmao@100tal.com>
2021-03-27 20:37:46 +0800

    Button Thin size mode

--- library/src/main/java/com/xhb/uibase/view/list/ItemBindings.java
+++ library/src/main/java/com/xhb/uibase/view/list/ItemBindings.java
@@ -7,6 +7,8 @@ import androidx.databinding.DataBindingUtil;
 public class ItemBindings {
 
     public static <T> ItemBinding<T> get(Context context, Object bindingOrLayout) {
+        if (bindingOrLayout == null)
+            return null;
         if (bindingOrLayout instanceof ItemBinding)
             return (ItemBinding) bindingOrLayout;
         return new UnitTypeItemBinding<>(context, (Integer) bindingOrLayout);
