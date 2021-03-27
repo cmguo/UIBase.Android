6af2c8d76ff39ad0bf535c09900a8dcb6111b527
guochunmao <guochunmao@100tal.com>
2021-03-27 16:14:41 +0800

    RoundIndicator: splitter

--- library/src/main/java/com/xhb/uibase/widget/tabbar/XHBRoundIndicator.kt
+++ library/src/main/java/com/xhb/uibase/widget/tabbar/XHBRoundIndicator.kt
@@ -3,9 +3,11 @@ package com.xhb.uibase.widget.tabbar
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.util.AttributeSet
 import com.xhb.uibase.R
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.WrapPagerIndicator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.model.PositionData
 
 class XHBRoundIndicator @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -46,6 +48,11 @@ class XHBRoundIndicator @JvmOverloads constructor(
     var color: Int
         get() = fillColor
         set(value) { fillColor = value }
+
+    var splitterWidth = 0f
+    var splitterHeight = 0f
+
+    var splitterColor = 0
     
     init {
         val style = if (defStyleAttr == 0) R.attr.roundIndicatorStyle else defStyleAttr
@@ -56,12 +63,39 @@ class XHBRoundIndicator @JvmOverloads constructor(
         roundRadius = a.getDimension(R.styleable.XHBRoundIndicator_borderRadius, roundRadius)
         shadowRadius = a.getDimension(R.styleable.XHBRoundIndicator_shadowRadius, shadowRadius)
         color = a.getColor(R.styleable.XHBRoundIndicator_color, color)
+        splitterWidth = context.resources.getDimension(R.dimen.xhb_round_indicator_splitter_width)
+        splitterHeight = a.getDimension(R.styleable.XHBRoundIndicator_splitterHeight, splitterHeight)
+        splitterColor = a.getColor(R.styleable.XHBRoundIndicator_splitterColor, splitterColor)
         a.recycle()
     }
 
-    override fun onDraw(canvas: Canvas?) {
+    private var splitterPositions: List<Float> = listOf()
+    private val splitterRect = RectF()
+
+    override fun onDraw(canvas: Canvas) {
+        if (splitterHeight > 0) {
+            paint.setShadowLayer(0f, 0f, 0f, 0)
+            paint.color = splitterColor
+            val dw = splitterWidth / 2
+            for (p in splitterPositions) {
+                splitterRect.left = p - dw
+                splitterRect.right = p + dw
+                canvas.drawRect(splitterRect, paint)
+            }
+        }
         paint.setShadowLayer(shadowRadius, 0f, 0f, Color.LTGRAY)
         super.onDraw(canvas)
+    }
+
+    override fun onPositionDataProvide(dataList: MutableList<PositionData>) {
+        super.onPositionDataProvide(dataList)
+        if (dataList.size > 0) {
+            splitterPositions = dataList.take(dataList.size - 1).map { it.mRight.toFloat() }
+            val c = (dataList[0].mContentTop + dataList[0].mContentBottom) / 2f
+            val dh = splitterHeight / 2
+            splitterRect.top = c - dh
+            splitterRect.bottom = c + dh
+        }
     }
 
 }
