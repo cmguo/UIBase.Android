737c03ba032b53fface0818da59cb73c3ed19dfe
jiangzhiguo <13340113093@163.com>
2021-04-07 15:29:12 +0800

    banner

---
+++ banner/src/main/java/com/eazy/banner/listener/OnBannerListener.java
@@ -0,0 +1,5 @@
+package com.eazy.banner.listener;
+
+public interface OnBannerListener {
+    public void OnBannerClick(int position);
+}
--- demo/src/main/java/com/eazy/uibase/demo/components/simple/ZCarouselComponent.kt
+++ demo/src/main/java/com/eazy/uibase/demo/components/simple/ZCarouselComponent.kt
@@ -3,8 +3,11 @@ package com.eazy.uibase.demo.components.simple
 import com.google.auto.service.AutoService
 import com.eazy.uibase.demo.R
 import com.eazy.uibase.demo.core.Component
+import com.eazy.uibase.demo.core.ComponentFragment
+import com.eazy.uibase.demo.core.annotation.Author
 
 @AutoService(Component::class)
+@Author("jiangzhiguo")
 class ZCarouselComponent : Component {
     override fun id(): Int {
         return R.id.component_z_carousels
@@ -26,4 +29,7 @@ class ZCarouselComponent : Component {
         return R.string.component_z_carousels_desc
     }
 
+    override fun fragmentClass(): Class<out ComponentFragment<*, *, *>?> {
+        return ZCarouselFragment::class.java
+    }
 }
